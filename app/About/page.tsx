8dab2d931dc7f18c3bd226338168afa06359c88e
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-01-03 14:45:44 +0500

    Add files via upload

---
+++ app/About/page.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+//import Image from 'next/image'
+
+
+const About = () => {
+  return (
+   
+       
+  
+  <>
+  <div>
+  
+  
+    <div className="grid grid-cols-2">
+  
+  <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+ </div>
+	
+    <div> what makes our pruduct different  </div>
+	  
+	
+ 
+		
+		
+		
+	    <div className="grid grid-cols-4">
+  
+  <div className='bg-blue-100  '>  jkghjhjh</div>
+ 
+		<div> hjgbhjhj</div>
+    	<div> hjgbhjhj</div>
+	  
+	<div> hjgbhjhj</div>
+ 
+		</div>	
+		
+		
+	<div> Populaer Product  </div>	
+	
+	
+		
+		    <div className="grid grid-cols-4">
+  
+  <div className='bg-green-100  '>  jkghjhjh</div>
+ 
+		<div> hjgbhjhj</div>
+    	<div> hjgbhjhj</div>
+	  <div> hjgbhjhj</div>
+    
+ 
+		</div>	
+		
+	  <div className="grid grid-cols-4">
+  
+  <div className='bg-yellow-100  '>  price</div>
+ 
+		<div> hjgbhjhj</div>
+    	<div> hjgbhjhj</div>
+	  <div> hjgbhjhj</div>
+    
+ 
+		</div>	
+  
+	
+
+	
+
+  
+	
+	</div>
+  </>  
+        
+       
+  )
+}
+
+export default About
---
+++ app/Productpages/page.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import Image from 'next/image'
+import img2 from "./whitesmall.jpg"
+
+const Productpages = () => {
+  return (
+   
+       
+  
+  <>
+  <div>
+  
+  <div> All Pruducts  </div>
+  
+    <div className="grid grid-cols-4">
+  
+  <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+ 
+ <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+   <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+ 
+ <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+   <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+ 
+ <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+ 
+ </div>
+	
+    
+	<div className="grid grid-cols-1">
+
+ <div> hjgbhjhj</div>
+ 
+ <div className='bg-blue-100  '>  jkghjhjh</div>
+ <div> hjgbhjhj</div>
+<div> 
+
+ <Image src={img2} alt="product0whitesmalljpg"
+       
+          className="z-50 rounded-t-[0.625rem] h-[100px] rounded-b-none w-full 
+          width: Fixed (1,321px)px;
+height: Hug (139px)px;
+top: 1107px;
+left: 300px;
+gap: 0px;
+justify: space-between;
+opacity: 70px;
+
+
+          "
+        />
+
+
+
+
+</div>
+
+
+</div>	
+	
+ 
+		
+		
+		
+	  
+  
+  
+  
+	
+
+	
+
+  
+	
+	</div>
+  </>  
+        
+       
+  )
+}
+
+export default Productpages
