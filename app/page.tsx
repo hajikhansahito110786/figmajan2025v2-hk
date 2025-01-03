24dce1089fe06bf0874217e7b5a8cc10d82d9b61
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2025-01-03 14:59:46 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+//import Image from "next/image";
+
+
+
+
+
+
+import Startupnew from "./startupnew";
+
+
+
+
+
+
+export default  function Home() {
+
+
+
+  
+
+ 
+  return (
+    
+ 
+     <div>
+	 <Startupnew/>  
+
+</div>
+
+   
+      
+     
+   
+  );
+}
